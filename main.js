543038569a1c37275d94478c8bf84802a9a9a12b
Atheer <atheerm96@googlemail.com>
2020-07-21 15:51:10 +0100

    add storing text in searchbar to variable upon submit

--- main.js
+++ main.js
@@ -1,20 +1,40 @@
-const searchButton = document.getElementById('searchButton');
-const imFeelingLucky = document.getElementById('imFeelingLucky');
+const form = document.querySelector('#form');
+form.addEventListener('submit', returnText);
+
+let myString = "";
+function returnText(e) {
+
+  e.preventDefault();
+
+
+    const tg = e.target;
+    text = tg.searchBar.value;
+    myString += text
+
+    console.log(myString)
+    return myString
+};
 
 
 
-searchButton.addEventListener("submit", function(e){
-  e.preventDefault()
-  let inputVal = "";
-  const tg = e.target;
-  submit = tg.searchButton.value
-  inputVal += submit
-  // inputVal = e.target.getElementById("myInput").value
-  console.log(inputVal)
+
+
+
+// const searchButton = document.getElementById('searchButton');
+// const imFeelingLucky = document.getElementById('imFeelingLucky');
+//
+// let inputVal = document.getElementById("searchBar").value;
+//
+// form.addEventListener("submit", function(e){
+//   e.preventDefault()
+  // const tg = e.target;
+  // submit = tg.searchButton.value
+  // inputVal += submit
+  // alert(inputVal)
   //Brings up search results
-});
-
-imFeelingLucky.addEventListener("submit", function(e){
-  e.preventDefault()
-  //Takes you to first Search Result
-})
+// });
+//
+// imFeelingLucky.addEventListener("submit", function(e){
+//   e.preventDefault()
+//   //Takes you to first Search Result
+// })
